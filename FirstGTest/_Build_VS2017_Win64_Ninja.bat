711af2e32e661a4bb8fc07f914a3cfd1bc743ddc
Thomas Lämmlein <thomaslaemmlein@gmail.com>
2018-10-21 17:29:45 +0200

    Build with ninja

---
+++ FirstGTest/_Build_VS2017_Win64_Ninja.bat
@@ -0,0 +1,21 @@
+@echo off
+
+call %~dp0\..\Build\Environment\SetEnvironment.bat
+
+call %~dp0\_Cleanup.bat
+
+cd %~dp0\build 
+
+cmake ..\  -G "Ninja" -DCMAKE_BUILD_TYPE=Release
+
+ninja
+
+cd ..
+
+echo The executable is located under %~dp0\build\bin
+
+rem cmake %~dp0 -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+rem nmake
+
+
